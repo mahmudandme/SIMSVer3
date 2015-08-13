b6e0e14eca12b646d53f93f8afee9e340f560074
mahmudandme <mailtommrr@gmail.com>
2015-08-13 20:10:33 +0600

    ete ete ete

---
+++ SIMS/DAL/Student/StudentDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using SIMS.Models;
+
+namespace SIMS.DAL.Student
+{
+    public class StudentDAL
+    {
+        public StudentModel GetStudentInformation(String studentId)
+        {
+            StudentModel studentModel = null;
+            string  query = String.Format("spGetTheStudentById");
+            using (SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings[1].ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query,connection))
+                {
+                    command.Parameters.Clear();
+                    command.Parameters.AddWithValue("@studentId", studentId);
+                    SqlDataReader rdr = command.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        studentModel = new StudentModel();
+                        
+                    }
+                }
+            }
+            return studentModel;
+        }
+    }
+}
---
+++ SIMS/Models/StudentModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SIMS.Models
+{
+    public class StudentModel
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Nationality { get; set; }
+        public string DepartmentName { get; set; }
+        public string Session { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string PresentAddress { get; set; }
+        public string PermanentAddress { get; set; }    
+    }
+}
